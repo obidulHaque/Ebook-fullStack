f00d2a7998972846574f649ab5d1a715600e8487
Obaidul Haque <obaidulhoqejoy@gmail.com>
2024-11-29 20:59:06 +0600

    New

--- Ebook Dashboard/src/App.tsx
+++ Ebook Dashboard/src/App.tsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className=" w-screen h-screen justify-center flex items-center ">
       {" "}
+      <h1>This is The demo </h1>
       <Button>click Me</Button>
     </div>
   );
